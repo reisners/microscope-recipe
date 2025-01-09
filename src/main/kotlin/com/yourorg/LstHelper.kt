4574a613ca9198149035fe43b5518795c2fd9e1f
Stefan Reisner <stefan.reisner@bolindalabs.com>
2025-01-09 19:39:08 +0200

    fix nullability

--- src/main/kotlin/com/yourorg/LstHelper.kt
+++ src/main/kotlin/com/yourorg/LstHelper.kt
@@ -4,20 +4,20 @@ import org.openrewrite.java.tree.Expression
 import org.openrewrite.java.tree.J
 import org.openrewrite.kotlin.tree.K
 
-fun List<J.Annotation>.asMapOfMaps(): Map<String, Map<String, Array<Any?>>> {
+fun List<J.Annotation>.asMapOfMaps(): Map<String, Map<String, Array<Any?>?>> {
     return this.associate { it: J.Annotation ->
         it.annotationType.toString() to (it.getArguments()?.asMap() ?: emptyMap())
     }
 }
 
-fun List<Expression>.asMap(): Map<String, Array<Any?>> {
+fun List<Expression>.asMap(): Map<String, Array<Any?>?> {
     return this.mapNotNull { expression ->
             when (expression) {
-                is J.Literal -> "value" to expression.extractValue()!!
+                is J.Literal -> "value" to expression.extractValue()
                 is J.Assignment ->
-                    expression.let { it.variable.toString() to it.getAssignment().extractValue()!! }
+                    expression.let { it.variable.toString() to it.getAssignment().extractValue() }
 
-                is K.ListLiteral -> "value" to expression.extractValue()!!
+                is K.ListLiteral -> "value" to expression.extractValue()
                 else -> null
             }
         }
